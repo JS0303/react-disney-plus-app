b2a5f00d760371171d6e9a3f52893bd0f1d760eb
js0303 <wlstjr0303@gmail.com>
2023-02-25 15:37:04 +0900

    Row 컴포넌트 슬라이드 기능 추가

--- src/components/Row.js
+++ src/components/Row.js
@@ -20,9 +20,16 @@ const Row = ({ title, id, fetchUrl }) => {
       <h2>{title}</h2>
       <div className="slider">
         <div className="slider__arrow-left">
-          <span className="arrow">{"<"}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
         </div>
-        <div id="{id}" className="row__posters">
+        <div id={id} className="row__posters">
           {movies.map((movie) => (
             <img
               key={movie.id}
@@ -34,9 +41,14 @@ const Row = ({ title, id, fetchUrl }) => {
           ))}
         </div>
         <div className="slider__arrow-right">
-          <div className="slider__arrow-left">
-            <span className="arrow">{">"}</span>
-          </div>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            {">"}
+          </span>
         </div>
       </div>
     </div>
