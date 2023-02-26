cd998607a5fe36a7e61d95f8c7b613772e5e87db
js0303 <wlstjr0303@gmail.com>
2023-02-26 15:23:16 +0900

    검색페이지 구현

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,7 @@
 import axios from "../../api/axios";
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./SearchPage.css";
 
 const SearchPage = () => {
   const [searchResults, setSearchResults] = useState([]);
@@ -11,6 +12,7 @@ const SearchPage = () => {
 
   let query = useQuery();
   const searchTerm = query.get("q");
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (searchTerm) {
@@ -29,6 +31,41 @@ const SearchPage = () => {
       console.log(error);
     }
   };
+
+  if (searchResults.length > 0) {
+    return (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return (
+              <div className="movie" key={movie.id}>
+                <div
+                  className="movie__column-poster"
+                  onClick={() => navigate(`/${movie.id}`)}
+                >
+                  <img
+                    src={movieImageUrl}
+                    alt="movie"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    );
+  } else {
+    return (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>찾고자 하는 검색어 "{searchTerm}" 에 맞는 영화가 없습니다.</p>
+        </div>
+      </section>
+    );
+  }
 
   return <div>SearchPage</div>;
 };
